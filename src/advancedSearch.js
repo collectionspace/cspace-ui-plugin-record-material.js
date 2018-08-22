129a0484697803ab526a349cf5cdf5d895a877b5
Ray Lee <rhlee@berkeley.edu>
2018-08-22 01:17:00 -0700

    CSPACE-7155: Add fields to adv search.

--- src/advancedSearch.js
+++ src/advancedSearch.js
@@ -52,6 +52,94 @@ export default (configContext) => {
         op: OP_EQ,
         path: 'ns2:materials_common/materialTermAttributionContributingGroupList/materialTermAttributionContributingGroup/materialTermAttributionContributingPerson',
       },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/commonForm',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/formTypeGroupList/formTypeGroup/formType',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/acousticalPropertyGroupList/acousticalPropertyGroup/acousticalPropertyType',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/durabilityPropertyGroupList/durabilityPropertyGroup/durabilityPropertyType',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/electricalPropertyGroupList/electricalPropertyGroup/electricalPropertyType',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/hygrothermalPropertyGroupList/hygrothermalPropertyGroup/hygrothermalPropertyType',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/mechanicalPropertyGroupList/mechanicalPropertyGroup/mechanicalPropertyType',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/opticalPropertyGroupList/opticalPropertyGroup/opticalPropertyType',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/sensorialPropertyGroupList/sensorialPropertyGroup/sensorialPropertyType',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/smartMaterialPropertyGroupList/smartMaterialPropertyGroup/smartMaterialPropertyType',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/additionalPropertyGroupList/additionalPropertyGroup/additionalPropertyType',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/recycledContentGroupList/recycledContentGroup/recycledContentQualifier',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/lifecycleComponentGroupList/lifecycleComponentGroup/lifecycleComponent',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/certificationCreditGroupList/certificationCreditGroup/certificationProgram',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/castingProcesses/castingProcess',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/joiningProcesses/joiningProcess',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/moldingProcesses/moldingProcess',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/surfacingProcesses/surfacingProcess',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/deformingProcesses/deformingProcess',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/machiningProcesses/machiningProcess',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/rapidPrototypingProcesses/rapidPrototypingProcess',
+      },
+      {
+        op: OP_EQ,
+        path: 'ns2:materials_common/additionalProcessGroupList/additionalProcessGroup/additionalProcess',
+      },
       ...extensions.core.advancedSearch,
     ],
   };
